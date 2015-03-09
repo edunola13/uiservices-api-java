2c0fea62826b270581b5263a0b17df9ddfcc90b4
edunola13 <edunola13@hotmail.com>
2015-03-09 19:25:34 -0300

    JSF Components

---
+++ src/com/tags/jsf/Theme.java
@@ -0,0 +1,37 @@
+package com.tags.jsf;
+
+import java.io.IOException;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import com.ui.ApiUi;
+
+@FacesComponent(createTag = true, namespace="http://www.edunola.com.ar/uicomponents", tagName="theme", value="theme")
+public class Theme extends UIComponentBase{
+	private String nombre= "base";
+	
+	@Override
+    public String getFamily() {        
+        return "EnolaUIServices";
+    }
+ 
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+    	ApiUi api= ApiUi.getInstance();
+    	
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write(api.theme(this.getNombre()));
+    }
+
+    
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
--- src/com/tags/jsf/statics/AlertMessage.java
+++ src/com/tags/jsf/statics/AlertMessage.java
@@ -1,43 +1,40 @@
-package com.tags.statics;
+package com.tags.jsf.statics;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.TagSupport;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 
 import com.ui.ApiUi;
 
-@SuppressWarnings("serial")
-public class AlertMessage extends TagSupport{
+@FacesComponent(createTag = true, namespace="http://www.edunola.com.ar/uicomponents", tagName="alertMessage", value="alertMessage")
+public class AlertMessage extends UIComponentBase{
 	private String type;
 	private String strong="";
 	private String message;
 	
 	@Override
-	public int doStartTag() throws JspException {
-		ApiUi api= ApiUi.getInstance();
+    public String getFamily() {        
+        return "EnolaUIServices";
+    }
+ 
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+    	ApiUi api= ApiUi.getInstance();
 		
 		//Armo un mapa con los valores de configuracion del Componente
 		Map<String, Object> valores= new HashMap<String, Object>();
 		valores.put("config.type", this.getType());
 		valores.put("config.strong", this.getStrong());
 		valores.put("config.message", this.getMessage());
-		
-		//Perform substr operation on string.
-		try {
-			//Get the writer object for output.
-			JspWriter out = pageContext.getOut();
-			//Imprimo el resultado en la JSP
-			out.println(api.imprimirComponente("alert_message", valores));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return SKIP_BODY;
-	}
+    	
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write(api.imprimirComponente("alert_message", valores));
+    }
 
 	public String getType() {
 		return type;
@@ -62,7 +59,4 @@ public class AlertMessage extends TagSupport{
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
-	
-	
 }
